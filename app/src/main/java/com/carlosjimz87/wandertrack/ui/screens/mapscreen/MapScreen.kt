a51aad87cb3a10f3532722e5196dab724bbc7187
Carlos Jiménez <carlosjimz87@gmail.com>
2025-04-27 01:57:03 +0200

    Set overlay (basic polygons) for visited countries

--- app/src/main/java/com/carlosjimz87/wandertrack/ui/screens/mapscreen/MapScreen.kt
+++ app/src/main/java/com/carlosjimz87/wandertrack/ui/screens/mapscreen/MapScreen.kt
@@ -1,13 +1,18 @@
 package com.carlosjimz87.wandertrack.ui.screens.mapscreen
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import com.carlosjimz87.wandertrack.R
+import com.carlosjimz87.wandertrack.ui.theme.AccentPinkDark
+import com.carlosjimz87.wandertrack.utils.getMockCountryBorderLatLng
 import com.carlosjimz87.wandertrack.utils.getMockCountryCodeFromLatLng
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
@@ -16,6 +21,7 @@ import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.MapType
 import com.google.maps.android.compose.MapUiSettings
+import com.google.maps.android.compose.Polygon
 import com.google.maps.android.compose.rememberCameraPositionState
 import org.koin.androidx.compose.koinViewModel
 
@@ -25,13 +31,19 @@ fun MapScreen(
     onCountryClicked: (String) -> Unit
 ) {
     val context = LocalContext.current
-    val mapStyleOptions = remember {
-        MapStyleOptions.loadRawResourceStyle(context, R.raw.map_style)
-    }
-    val visitedCountries by viewModel.visitedCountries.collectAsState()
-    val cameraPositionState = rememberCameraPositionState{
+    val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(LatLng(20.0, 0.0), 2f)
     }
+    val countries by viewModel.visitedCountries.collectAsState()
+    val isDarkTheme = isSystemInDarkTheme()
+    val mapStyleOptions = remember(isDarkTheme) {
+        if (isDarkTheme) {
+            MapStyleOptions.loadRawResourceStyle(context, R.raw.map_style_night)
+        } else {
+            MapStyleOptions.loadRawResourceStyle(context, R.raw.map_style)
+        }
+    }
+    val visitedColor = AccentPinkDark
 
     GoogleMap(
         modifier = Modifier.fillMaxSize(),
@@ -47,11 +59,19 @@ fun MapScreen(
         ),
         onMapClick = { latLng: LatLng ->
             val countryCode = getMockCountryCodeFromLatLng(latLng)
-            countryCode?.let{ onCountryClicked(it) }
+            countryCode?.let { onCountryClicked(it) }
         }
     ) {
-        visitedCountries.forEach {
-            // draw overlay for each visited country
+        countries.filter { it.visited }.forEach { country ->
+            val borderPoints = getMockCountryBorderLatLng(country.code)
+            if (borderPoints.isNotEmpty()) {
+                Polygon(
+                    points = getMockCountryBorderLatLng(country.code),
+                    fillColor = visitedColor.copy(alpha = 0.5f),
+                    strokeColor = visitedColor,
+                    strokeWidth = 4f
+                )
+            }
         }
 
     }
--- app/src/main/java/com/carlosjimz87/wandertrack/utils/Extensions.kt
+++ app/src/main/java/com/carlosjimz87/wandertrack/utils/Extensions.kt
@@ -8,3 +8,31 @@ fun getMockCountryCodeFromLatLng(latLng: LatLng): String? {
         "PL"
     } else null
 }
+
+fun getMockCountryBorderLatLng(code: String): List<LatLng> {
+    return when (code) {
+        "ES" -> listOf(
+            LatLng(43.7914, -1.5561),
+            LatLng(43.3947, 3.1814),
+            LatLng(40.8547, 3.1044),
+            LatLng(36.0000, -5.6000),
+            LatLng(36.6500, -9.1000),
+            LatLng(43.7914, -1.5561) // Cierra el polígono
+        )
+        "IT" -> listOf(
+            LatLng(47.092, 6.614),
+            LatLng(47.092, 13.806),
+            LatLng(36.651, 13.806),
+            LatLng(36.651, 6.614),
+            LatLng(47.092, 6.614)
+        )
+        "FR" -> listOf(
+            LatLng(51.0891, 2.5135),
+            LatLng(51.0891, 8.2275),
+            LatLng(42.3314, 8.2275),
+            LatLng(42.3314, 2.5135),
+            LatLng(51.0891, 2.5135)
+        )
+        else -> emptyList()
+    }
+}
