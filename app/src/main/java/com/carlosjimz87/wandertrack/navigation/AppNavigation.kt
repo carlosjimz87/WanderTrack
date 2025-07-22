0377d62c0ad20db706f210c3fcc119dc578ce6a4
Carlos Jiménez <carlosjimz87@gmail.com>
2025-07-22 18:52:56 +0200

    Add Nav animations

--- app/src/main/java/com/carlosjimz87/wandertrack/navigation/AppNavigation.kt
+++ app/src/main/java/com/carlosjimz87/wandertrack/navigation/AppNavigation.kt
@@ -1,6 +1,11 @@
 package com.carlosjimz87.wandertrack.navigation
 
 import android.widget.Toast
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.with
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -15,6 +20,8 @@ import com.carlosjimz87.wandertrack.ui.screens.splash.SplashScreen
 import com.carlosjimz87.wandertrack.utils.Logger
 import org.koin.androidx.compose.koinViewModel
 
+
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun AppNavigation(authViewModel: AuthViewModel = koinViewModel()) {
     val controller = rememberMyNavController()
@@ -23,78 +30,76 @@ fun AppNavigation(authViewModel: AuthViewModel = koinViewModel()) {
     val firebaseUser = authViewModel.authState.collectAsState().value
 
     LaunchedEffect(currentScreen) {
-        Logger.d("Navigated to: ${'$'}currentScreen")
+        Logger.d("Navigated to: $currentScreen")
     }
+
     LaunchedEffect(firebaseUser) {
         if ((currentScreen is Screen.Map || currentScreen is Screen.Profile) && firebaseUser == null) {
             controller.replace(Screen.Auth)
         }
     }
 
-    when (val screen = currentScreen) {
-        is Screen.Splash -> SplashScreen(
-            onSplashFinished = {
-                if (firebaseUser != null) {
-                    controller.replace(Screen.Map(firebaseUser.uid))
-                } else {
-                    controller.replace(Screen.Auth)
-                }
-            }
-        )
-
-        is Screen.Auth -> AuthScreen(
-            onGetStartedClick = { controller.navigate(Screen.SignUp) },
-            onSignInClick = { controller.navigate(Screen.Login) }
-        )
-
-        is Screen.Login -> {
-            LaunchedEffect(firebaseUser) {
-                if (firebaseUser != null && firebaseUser.isEmailVerified) {
-                    controller.replace(Screen.Map(firebaseUser.uid))
-                }
-            }
-
-            LoginScreen(
-                onGoogleIdTokenReceived = { idToken ->
-                    authViewModel.loginWithGoogle(idToken) { success, msg ->
-                        if (!success) {
-                            Toast.makeText(context, "Authentication error", Toast.LENGTH_SHORT)
-                                .show()
-                        }
+    AnimatedContent(
+        targetState = currentScreen,
+        transitionSpec = { fadeIn() with fadeOut() }
+    ) { screen ->
+        when (screen) {
+            is Screen.Splash -> SplashScreen(
+                onSplashFinished = {
+                    if (firebaseUser != null) {
+                        controller.replace(Screen.Map(firebaseUser.uid))
+                    } else {
+                        controller.replace(Screen.Auth)
                     }
-                },
-                onBack = { controller.replace(Screen.Auth) }
-            )
-        }
-
-        is Screen.SignUp -> {
-            LaunchedEffect(firebaseUser) {
-                if (firebaseUser != null) {
-                    controller.replace(Screen.Map(firebaseUser.uid))
-                }
-            }
-            SignUpScreen(
-                onNavigateToLogin = {
-                    controller.replace(Screen.Login)
-                },
-                onBack = {
-                    controller.replace(Screen.Auth)
                 }
             )
-        }
 
-        is Screen.Map -> MapScreen(
-            userId = screen.userId,
-            onProfileClick = { controller.navigate(Screen.Profile) }
-        )
+            is Screen.Auth -> AuthScreen(
+                onGetStartedClick = { controller.navigate(Screen.SignUp) },
+                onSignInClick = { controller.navigate(Screen.Login) }
+            )
 
-        is Screen.Profile -> ProfileScreen(
-            onLogout = {
-                controller.replace(Screen.Auth)
-            },
-            onBack = {
-                controller.pop()
+            is Screen.Login -> {
+                LaunchedEffect(firebaseUser) {
+                    if (firebaseUser != null && firebaseUser.isEmailVerified) {
+                        controller.replace(Screen.Map(firebaseUser.uid))
+                    }
+                }
+
+                LoginScreen(
+                    onGoogleIdTokenReceived = { idToken ->
+                        authViewModel.loginWithGoogle(idToken) { success, msg ->
+                            if (!success) {
+                                Toast.makeText(context, "Authentication error", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    },
+                    onBack = { controller.replace(Screen.Auth) }
+                )
             }
-        )
+
+            is Screen.SignUp -> {
+                LaunchedEffect(firebaseUser) {
+                    if (firebaseUser != null) {
+                        controller.replace(Screen.Map(firebaseUser.uid))
+                    }
+                }
+
+                SignUpScreen(
+                    onNavigateToLogin = { controller.replace(Screen.Login) },
+                    onBack = { controller.replace(Screen.Auth) }
+                )
+            }
+
+            is Screen.Map -> MapScreen(
+                userId = screen.userId,
+                onProfileClick = { controller.navigate(Screen.Profile) }
+            )
+
+            is Screen.Profile -> ProfileScreen(
+                onLogout = { controller.replace(Screen.Auth) },
+                onBack = { controller.pop() }
+            )
+        }
     }
 }
